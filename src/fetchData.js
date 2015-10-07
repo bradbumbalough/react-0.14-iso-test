3d125613885667490093091afc61a677bcbbc15b
Brad Bumbalough <bradley.bumbalough@gmail.com>
2015-10-07 09:19:28 -0400

    removed getComponents() function in fetchData

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,16 +1,7 @@
 var all = require('when/keys').all
 
-function getComponents(props) {
-  // return an array of the rendered components
-  return props.components !== undefined ?
-    props.components :
-    props.routes.map((route) => {
-      return route.component
-    })
-}
-
 var fetchData = module.exports = (renderProps) => {
-  return all(getComponents(renderProps).filter((component) => {
+  return all(renderProps.components.filter((component) => {
     return component.loadData
   }).reduce((promises, component) => {
     promises[component.displayName] = component.loadData(renderProps);
